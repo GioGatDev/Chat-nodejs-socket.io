b973e00d1e2a680b1df5bf7978a9c02f5b608907
Giovanni <giovannigatfut@gmail.com>
2018-06-04 03:58:09 -0500

    Creando estructura de mensaje y recepcion

--- client/main.js
+++ client/main.js
@@ -1,1 +1,22 @@
- var socket = io.connect('http://192.168.1.133:6677',{'forceNew':true})
+ var socket = io.connect('http://192.168.1.133:6677',{'forceNew':true});
+
+ socket.on('messages', function (data) {
+ 	// recibe los datos que llegan del servidor 
+ 	console.log(data);
+ 	render(data);
+ });
+
+ function render(data) {
+ 	// body...
+ 	var html = data.map(function(message, index){
+ 		return(`
+ 			<div class="message">
+ 			<strong>${message.nickname} </strong>
+ 			<p>${message.text}</p>
+ 			</div>
+ 			`);//recibe el nombre del usuario y el mensage
+
+ 	}).join(' '); //dar espacio entre parrafo
+
+ 	document.getElementById('mensajes').innerHTML = html;
+ }
--- server/index.js
+++ server/index.js
@@ -5,12 +5,20 @@ var io = require('socket.io')(server);
 
 app.use(express.static('client'));
 
+var messages = [{
+	id:1,
+	text: 'Bienvenido al chat',
+	nickname: 'Admin'
+}];
+
 app.get('/hola-a-todos', function(req, res){
 	res.status(200).send('hola a todos desde una ruta');
 });
 /*Conexiónes con el servidor obteniendo la IP de donde se está conectando*/
 io.on('connection', function(socket){
 	console.log("El Cliente con IP:"+socket.handshake.address+" se ha conectado...");
+
+	socket.emit('messages', messages);
 });
 
 server.listen(6677, function() {
